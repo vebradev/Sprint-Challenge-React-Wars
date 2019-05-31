b0c6727cbaeed834a50fb24bbf2de61bbf879299
vebradev <justinas@vebra.dev>
2019-05-31 12:00:53 +0300

    Add pagination functionality to App.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CharacterList from "./components/CharacterList";
+import Pagination from "./components/Pagination";
 import "./App.css";
 
 class App extends Component {
@@ -21,18 +22,43 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        console.log(data);
+        this.setState({ 
+          starwarsChars: data.results,
+          next: data.next,
+          previous: data.previous,
+        });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
+  previous = () => {
+    console.log('previous');
+    if (this.state.previous !== null) {
+      this.getCharacters(this.state.previous)
+    }
+  }
+
+  next = () => {
+    console.log('next');
+    this.getCharacters(this.state.next)
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <CharacterList data={this.state.starwarsChars} />
+        <CharacterList
+          data={this.state.starwarsChars}
+          films={this.state.films}
+        />
+
+        <Pagination
+          previous={this.previous}
+          next={this.next}
+        />
       </div>
     );
   }
