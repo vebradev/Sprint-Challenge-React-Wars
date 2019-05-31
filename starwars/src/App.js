ac604bb8cef279a892ea00ae151be0ee4f4528bc
vebradev <justinas@vebra.dev>
2019-05-31 11:01:36 +0300

    Render CharacterList in the App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import CharacterList from "./components/CharacterList";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      films: []
     };
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters("https://swapi.co/api/people/");
   }
 
   getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
         return res.json();
@@ -33,6 +32,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharacterList data={this.state.starwarsChars} />
       </div>
     );
   }
